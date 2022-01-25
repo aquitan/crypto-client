d855f796e2761bbb445af3bd3206632b61fe542c
noo8xl <niko8elich@gmail.com>
2022-01-25 17:13:07 +0200

    update db dump

--- crypto_app.sql
+++ crypto_app.sql
@@ -59,10 +59,12 @@ CREATE TABLE `user_auth` (
   `isBanned` tinyint(1) NOT NULL,
   `isActivated` tinyint(1) NOT NULL,
   `activationLink` varchar(255) NOT NULL,
-  `domainName` varchar(255) NOT NULL,
-  `dateOfEntry` varchar(50) NOT NULL,
+  `self_registration` varchar(30) NOT NULL,
+  `promocode` varchar(50) NOT NULL,
+  `domain_name` varchar(255) NOT NULL,
+  `date_of_entry` varchar(50) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,8 +73,107 @@ CREATE TABLE `user_auth` (
 
 LOCK TABLES `user_auth` WRITE;
 /*!40000 ALTER TABLE `user_auth` DISABLE KEYS */;
-INSERT INTO `user_auth` VALUES (1,'onegin343@ukr.net','qwe12345','myName',1,0,0,0,1,'77df1d49-dec6-433f-81c8-4b786a4d37d3','opentrade.cc','2022-01-20 21:15:11');
 /*!40000 ALTER TABLE `user_auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_kyc`
+--
+
+DROP TABLE IF EXISTS `user_kyc`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_kyc` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone_number` varchar(50) NOT NULL,
+  `date_of_birth` varchar(50) NOT NULL,
+  `document_number` varchar(100) NOT NULL,
+  `main_address` varchar(255) NOT NULL,
+  `sub_address` varchar(255) DEFAULT NULL,
+  `city` varchar(30) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `zip_code` int NOT NULL,
+  `document_type` varchar(50) NOT NULL,
+  `kyc_status` varchar(30) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_kyc_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_auth` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_kyc`
+--
+
+LOCK TABLES `user_kyc` WRITE;
+/*!40000 ALTER TABLE `user_kyc` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_kyc` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_logs`
+--
+
+DROP TABLE IF EXISTS `user_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_logs` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `ip_address` varchar(150) NOT NULL,
+  `request_city` varchar(100) NOT NULL,
+  `country_name` varchar(50) NOT NULL,
+  `location` varchar(150) NOT NULL,
+  `action_date` varchar(50) NOT NULL,
+  `user_action` varchar(255) NOT NULL,
+  `user_domain` varchar(255) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `user_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_auth` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_logs`
+--
+
+LOCK TABLES `user_logs` WRITE;
+/*!40000 ALTER TABLE `user_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_promocode`
+--
+
+DROP TABLE IF EXISTS `user_promocode`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_promocode` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `code` varchar(30) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `value` float NOT NULL,
+  `staff_user_id` int NOT NULL,
+  `domain_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `staff_user_id` (`staff_user_id`),
+  CONSTRAINT `user_promocode_ibfk_1` FOREIGN KEY (`staff_user_id`) REFERENCES `user_auth` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_promocode`
+--
+
+LOCK TABLES `user_promocode` WRITE;
+/*!40000 ALTER TABLE `user_promocode` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_promocode` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -84,4 +185,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-20 20:42:57
+-- Dump completed on 2022-01-25 17:12:41
