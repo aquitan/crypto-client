00ec408d581cfa2d36861c616f5d83905ec66c72
noo8xl <niko8elich@gmail.com>
2022-08-04 17:50:33 +0300

    base crypto.lib usage

---
+++ server/services/crypto.lib/crypto.main.ts
@@ -0,0 +1,2 @@
+import * as walletGenerator from './baseUsage/generate_wallets'
+
---
+++ server/services/crypto.lib/lib.helper/transactionData.interface.ts
@@ -0,0 +1,10 @@
+
+
+export interface transactionData {
+  coinName: string
+  recruiterFee: number
+  staffFee: number
+  staffWallet: string
+  recruiterWallet: string | null
+  balanceSum: number
+}
