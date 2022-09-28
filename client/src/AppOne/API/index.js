b6e14f4a71e4afa7c61550650e48b4d4c5b49e31
aquitan <aquitan@mail.ru>
2022-09-28 15:05:29 +0300

    cors fix build

--- client/src/AppOne/API/index.js
+++ client/src/AppOne/API/index.js
@@ -16,32 +16,35 @@ export const coinsApi = axios.create({
 
 export const $api = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'Access-Control-Allow-Origin': 'http://45.67.56.28'
+    }
 })
 
-// $api.interceptors.request.use((config) => {
-//     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-//     return config
-// })
-//
-// $api.interceptors.response.use((config) => {
-//     return config
-// }, async (error) => {
-//     const originalRequest = error.config
-//     if (error.response.status === 401 && error.config && !error.config._isRetry) {
-//         originalRequest._isRetry = true
-//         console.log('try 1')
-//         try {
-//             console.log('retry 2')
-//             const response = await axios.get(`${BASE_URL}/refresh`, {withCredentials: true})
-//             localStorage.setItem('token', response.data.accessToken)
-//             return $api.request(originalRequest)
-//         } catch(e) {
-//             console.log(e.message)
-//             console.log('Не авторизованный пользователь')
-//         }
-//     }
-//     throw error
-// })
+$api.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    return config
+})
+
+$api.interceptors.response.use((config) => {
+    return config
+}, async (error) => {
+    const originalRequest = error.config
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true
+        console.log('try 1')
+        try {
+            console.log('retry 2')
+            const response = await axios.get(`${BASE_URL}/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken)
+            return $api.request(originalRequest)
+        } catch(e) {
+            console.log(e.message)
+            console.log('Не авторизованный пользователь')
+        }
+    }
+    throw error
+})
 
 export default $api;
