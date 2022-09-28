5d8a2c3d8749b99a29068548f13cf53dbb0e7ba2
aquitan <aquitan@mail.ru>
2022-09-28 14:54:29 +0300

    cors fix build

--- client/src/AppOne/API/index.js
+++ client/src/AppOne/API/index.js
@@ -19,29 +19,29 @@ export const $api = axios.create({
     withCredentials: true
 })
 
-$api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-    return config
-})
-
-$api.interceptors.response.use((config) => {
-    return config
-}, async (error) => {
-    const originalRequest = error.config
-    if (error.response.status === 401 && error.config && !error.config._isRetry) {
-        originalRequest._isRetry = true
-        console.log('try 1')
-        try {
-            console.log('retry 2')
-            const response = await axios.get(`${BASE_URL}/refresh`, {withCredentials: true})
-            localStorage.setItem('token', response.data.accessToken)
-            return $api.request(originalRequest)
-        } catch(e) {
-            console.log(e.message)
-            console.log('Не авторизованный пользователь')
-        }
-    }
-    throw error
-})
+// $api.interceptors.request.use((config) => {
+//     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+//     return config
+// })
+//
+// $api.interceptors.response.use((config) => {
+//     return config
+// }, async (error) => {
+//     const originalRequest = error.config
+//     if (error.response.status === 401 && error.config && !error.config._isRetry) {
+//         originalRequest._isRetry = true
+//         console.log('try 1')
+//         try {
+//             console.log('retry 2')
+//             const response = await axios.get(`${BASE_URL}/refresh`, {withCredentials: true})
+//             localStorage.setItem('token', response.data.accessToken)
+//             return $api.request(originalRequest)
+//         } catch(e) {
+//             console.log(e.message)
+//             console.log('Не авторизованный пользователь')
+//         }
+//     }
+//     throw error
+// })
 
 export default $api;
