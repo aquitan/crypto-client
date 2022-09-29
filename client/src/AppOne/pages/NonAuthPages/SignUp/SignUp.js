79fbf4b28d1910e9773a941268b7c75ee2105baf
aquitan <aquitan@mail.ru>
2022-09-29 10:13:11 +0300

    fixes on server

--- client/src/AppOne/pages/NonAuthPages/SignUp/SignUp.js
+++ client/src/AppOne/pages/NonAuthPages/SignUp/SignUp.js
@@ -89,7 +89,7 @@ const SignUp = () => {
         delete geoData.id
         delete geoData.userAction
         geoData.promocode = data.promocode
-        geoData.domainName = store.domain.fullDomainName
+        geoData.domainName = '167'
         geoData.doubleDeposit = store.domain.domainParams.doubleDeposit
         geoData.depositFee = store.domain.domainParams.depositFee
         geoData.minDepositSum = store.domain.domainParams.minDepositSum
