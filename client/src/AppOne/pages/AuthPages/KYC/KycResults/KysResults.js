eb06686cf22c64c664248da17364d8418a6a8274
aquitan <aquitan@mail.ru>
2022-10-27 11:59:49 +0300

    fixes on server

--- client/src/AppOne/pages/AuthPages/KYC/KycResults/KysResults.js
+++ client/src/AppOne/pages/AuthPages/KYC/KycResults/KysResults.js
@@ -1,13 +1,25 @@
+import { faCheckCircle, faExclamationCircle, faStopCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const KycResults = (props) => {
     const checkKycResult = () => {
         if (props.status === 'complete') {
-            return <h1>You've been Approved!</h1>
+            return <>
+                <h1>You've been Approved! <FontAwesomeIcon color='green' icon={faCheckCircle} /></h1>
+            </>
         } else if (props.status === 'rejected') {
-            return <h1>Your verification was Rejected!</h1>
+            return <>
+                <h1>Your verification was Rejected! <FontAwesomeIcon color='tomato' icon={faStopCircle} /></h1>
+                
+            </>
         } else {
-            return <h1>Pending verification!</h1>
+            return (<>
+                <h1>Pending verification! <FontAwesomeIcon color='orange' icon={faExclamationCircle} /></h1>
+                
+            </>
+
+            )
         }
     }
     return (
