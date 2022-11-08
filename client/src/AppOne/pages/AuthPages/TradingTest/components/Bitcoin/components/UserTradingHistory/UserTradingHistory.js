61a03c06cdffd986e644f40ab975b0d620922c6c
aquitan <aquitan@mail.ru>
2022-11-08 21:27:18 +0300

    fixes on server

--- client/src/AppOne/pages/AuthPages/TradingTest/components/Bitcoin/components/UserTradingHistory/UserTradingHistory.js
+++ client/src/AppOne/pages/AuthPages/TradingTest/components/Bitcoin/components/UserTradingHistory/UserTradingHistory.js
@@ -3,15 +3,28 @@ import {Col, Row} from 'react-bootstrap';
 import Button from '../../../../../../../components/UI/Button/Button';
 
 const UserTradingHistory = ({email, domain, date, coinName, coinValue, valueInUsdt, coinRate, orderType, orderStatus, onCancelOrder, id}) => {
+
+  const setOrderDate = () => {
+    let d = new Date(date * 1000).toLocaleString('EN-en')
+    return d
+  } 
+
     return (
-      <Row style={{fontSize: 12, alignItems: 'center', color: orderType ? 'green' : 'tomato', padding: '10px 0', borderBottom: '1px solid #fff'}}>
-          <Col>{email}</Col>
-          <Col>{domain}</Col>
-          <Col>{date}</Col>
-          <Col>{coinName}</Col>
-          <Col>{coinValue.toFixed(5)}</Col>
-          <Col>{valueInUsdt.toFixed(5)}</Col>
-          <Col>$ {coinRate}</Col>
+      <Row style={{fontSize: 12, alignItems: 'center', color: orderType ? 'green' : 'tomato', padding: '10px 0', borderBottom: '1px solid #eee'}}>
+          <Col className='d-none d-lg-block'>{email}</Col>
+          <Col className='d-none d-lg-block'>{domain}</Col>
+          <Col className='d-none d-lg-block'>{setOrderDate()}</Col>
+          <Col>
+            <div>
+              <b>{coinName}</b> {coinValue.toFixed(5)}
+            </div>
+            <div>
+              <b>Rate:</b> ${valueInUsdt.toFixed(5)}
+            </div>
+            <div>
+              <b>Price: ${coinRate}</b>
+            </div>
+          </Col>
           <Col>{orderStatus ? 'Active' : 'Completed'}</Col>
           <Col><Button onClick={() => onCancelOrder(id)} style={{height: 30, fontSize: 12}} classname='btnOrange'>Cancel</Button></Col>
       </Row>
